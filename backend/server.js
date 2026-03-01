81ac1f2c33b20acefde12be51af64d8a663a47cf
indrajapatil <indrajapatil2204@gmail.com>
2026-03-01 22:13:53 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://localhost:3001',
+    'http://127.0.0.1:3001',
     /\.vercel\.app$/,  // Allow all vercel.app subdomains
   ],
   credentials: true,
